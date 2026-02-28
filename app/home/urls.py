b0f1d1376690a06ce12fa1fc1794a85c6ce35815
Jhones Soares Neves <jhones.soares.neves@gmail.com>
2026-02-28 09:03:54 -0300

    Pagina home completa e inicio da pagina serviços

--- app/home/urls.py
+++ app/home/urls.py
@@ -6,5 +6,6 @@ app_name = "home"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("services", views.services, name="services"),
     # path("projeto/<int:id>/", views.project, name="projeto"),
 ]
--- app/home/views.py
+++ app/home/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from .models import About
+from .models import About, Services
 
 
 def home(request: HttpRequest) -> HttpResponse:
     abouts = About.objects.all().order_by("id")
     return render(request, "home/pages/home.html", context={"abouts": abouts})
+
+
+def services(request: HttpRequest) -> HttpResponse:
+    services = Services.objects.all().order_by("id")
+    return render(request, "home/pages/services.html", context={"services": services})
